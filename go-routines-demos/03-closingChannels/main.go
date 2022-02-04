b203e13a237bd028b7eeba226901a1b960f83fdf
BRUGUSOFT <kiran.paturi@brugu.io>
2022-02-04 15:07:07 +0530

    Channels with deadlock capacity adn length

---
+++ go-routines-demos/03-closingChannels/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func myfunc(channel chan string){
+	for i:=0;i<4;i++{
+		channel <- "Go Lang is Awesome"
+	}
+	fmt.Println("Lengh of the channel is", len(channel))
+	close(channel)
+}
+func main (){
+	c := make(chan string,5)
+	go myfunc(c)
+	//my func is acting as the data publisher
+	fmt.Println("Capacity of the channel is", cap(c))
+	for {
+		res, ok  := <-c
+		if !ok{
+			fmt.Println("Channel is closed", ok)
+			break
+		}
+		fmt.Println("Channel is open and data is", res,ok)
+	}
+}
