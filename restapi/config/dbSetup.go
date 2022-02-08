58d185261f79da1bf65df667a821ea2b84f41a85
BRUGUSOFT <kiran.paturi@brugu.io>
2022-02-08 11:28:09 +0530

    Added code

--- restapi/config/dbSetup.go
+++ restapi/config/dbSetup.go
@@ -1,16 +1,36 @@
 package config
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func ConnectDB() *mongo.Client {
+	//Get the instance of the datbase client.
 	client, err := mongo.NewClient(options.Client().ApplyURI(GetMongoDBURI()))
-	if err!=nil{
+	if err != nil {
 		fmt.Println("Error in Connecting to Mongo db Database")
 	}
-	return client;
+	fmt.Println("Database getting connected")
+	//Set the context to run the client in the background
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	
+	//Connect the client and run the background
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//ping the database
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB")
+
+	return client
 }
--- restapi/main.go
+++ restapi/main.go
@@ -1,23 +1,26 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	// "fmt"
+	// "log"
 	"multifileapp/config"
-	"multifileapp/routes"
-	"net/http"
-
-	"github.com/gorilla/mux"
+	// "multifileapp/routes"
+	// "net/http"
+	// "github.com/gorilla/mux"
 )
 
 func main() {
-	fmt.Println(config.SetupPort())
-	router := mux.NewRouter() // create routes
-	// call the route configuration
-	
-	// construct routing table
-	routes.Routes(router)
-	
-	log.Fatal(http.ListenAndServe(config.SetupPort(), router))
+	checkDBConnection()
+	// fmt.Println(config.SetupPort())
+	// router := mux.NewRouter() // create routes
+	// // call the route configuration
 
+	// // construct routing table
+	// routes.Routes(router)
+
+	// log.Fatal(http.ListenAndServe(config.SetupPort(), router))
+
+}
+func checkDBConnection() {
+	config.ConnectDB()
 }
